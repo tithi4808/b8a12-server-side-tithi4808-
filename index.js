a850b1c38b3d7cb7ace431706c98b0b791f1b9dd
tithi2149 <tanyasultana60@gmail.com>
2023-12-04 20:24:03 +0600

    updated

--- index.js
+++ index.js
@@ -328,8 +328,8 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     
 
